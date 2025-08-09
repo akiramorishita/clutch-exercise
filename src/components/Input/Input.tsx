2be46d20e2117251d3ff3cdb8371e6c1306a7ea9
Vitor Akira Morishita <vitor.morishita@gmail.com>
2025-08-09 00:44:15 -0300

    Update input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,20 @@
+import classNames from 'classnames';
 import './Input.css';
 const Input = ({
   placeholder = 'Placeholder',
   id,
+  value,
   ...props
 }: React.InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <div className="clutch-input__container">
       <input id={id} type="text" placeholder="" className="clutch-input__input" {...props} />
-      <label htmlFor={id} className="clutch-input__label">
+      <label
+        htmlFor={id}
+        className={classNames('clutch-input__label', {
+          'clutch-input__label--minimized': value
+        })}
+      >
         {placeholder}
       </label>
     </div>
