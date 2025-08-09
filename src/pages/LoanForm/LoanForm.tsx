a7e2d1370164e66f0577b4eba5f13db34a08dab1
Vitor Akira Morishita <vitor.morishita@gmail.com>
2025-08-09 12:16:44 -0300

    Fix footer position

--- src/pages/LoanForm/LoanForm.tsx
+++ src/pages/LoanForm/LoanForm.tsx
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer/Footer';
 import Button from '../../components/Button/Button';
 import Select from '../../components/Select/Select';
 import { useState } from 'react';
+import UserCostInfo from './components/UserCostInfo';
 
 const LoanForm = () => {
   const [select, setSelect] = useState('');
@@ -16,14 +17,8 @@ const LoanForm = () => {
 
       <div className="clutch-loanForm__container">
         <form className="clutch-loanForm__form">
-          <h1 className="text-gray-500">Loan Form</h1>
+          <h1 className="text-gray-500">Loan Information</h1>
           <div className="clutch-loanForm__fields">
-            <Input
-              id="totalLoanAmount"
-              placeholder="Total Loan Amount"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
             <Select
               id="loanPurpose"
               label="Loan Purpose"
@@ -36,6 +31,25 @@ const LoanForm = () => {
                 { value: 'apiError', label: 'API Error' }
               ]}
             />
+            <Input
+              id="totalLoanAmount"
+              placeholder="Total Loan Amount"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <Select
+              id="loanTerm"
+              label="Loan Term (months)"
+              placeholder="Select an option"
+              onChange={setSelect}
+              value={select}
+              options={[
+                { value: '12', label: '12 months' },
+                { value: '24', label: '24 months' },
+                { value: '36', label: '36 months' },
+                { value: '48', label: '48 months' }
+              ]}
+            />
             <small>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis dictum dignissim.
               In hac habitasse platea dictumst. Duis at efficitur dolor. Nulla id venenatis lectus,
@@ -43,9 +57,12 @@ const LoanForm = () => {
               Vestibulum a cursus eros, et tempor odio. Integer ut lobortis metus. Curabitur
               elementum sapien quam, lobortis blandit ipsum varius at.
             </small>
-            <div>
-              <div />
-            </div>
+            <UserCostInfo
+              data={[
+                { title: 'Monthly payment', value: '$85' },
+                { title: 'APR', value: '2.49%' }
+              ]}
+            />
           </div>
         </form>
       </div>
